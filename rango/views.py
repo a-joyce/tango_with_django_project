74720d7ee2ff5704d6d752b84e803604bceb66d7
Alexa Joyce <2241724j@student.gla.ac.uk>
2018-02-08 23:50:16 +0000

    incl chp8- more templates

--- rango/views.py
+++ rango/views.py
@@ -28,9 +28,11 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    # Return HttpResponse("Rango says here is the about page.")
-
-    return render(request, 'rango/about.html')
+    # prints out whether the method is a GET or a POST
+    print(request.method)
+    # prints out the user name, if no one is logged in it prints `AnonymousUser`
+    print(request.user)
+    return render(request, 'rango/about.html', {})
 
 def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass # to the template rendering engine.
